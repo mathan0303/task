c974394fdae9df1173bfa49549c0e88a4640bba0
mathan0303 <51322160+mathan0303@users.noreply.github.com>
2019-11-21 14:37:22 +0530

    Update colordetect.py

--- colordetect.py
+++ colordetect.py
@@ -4,20 +4,14 @@ import knn_classifier
 import os
 import os.path
 
-# read the test image
 source_image = cv2.imread('redshirt.jpg')
 prediction = 'n.a.'
-
-# checking whether the training data is ready
 PATH = './training.data'
-
 if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
     print ('training data is ready, classifier is loading...')
 else:
     open('training.data', 'w')
     feature.training()
-
-# get the prediction
 feature.test_image(source_image)
 prediction = knn_classifier.main('training.data', 'test.data')
 cv2.putText(
@@ -28,7 +22,5 @@ cv2.putText(
     1,
     50,
     )
-
-# Display the resulting frame
 cv2.imshow('color classifier', source_image)
 cv2.waitKey(0)
