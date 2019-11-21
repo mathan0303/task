c01b6b6190b3edf3b0f429b6a831496b4dad75f1
mathan0303 <51322160+mathan0303@users.noreply.github.com>
2019-11-21 14:36:11 +0530

    Update feature.py

--- feature.py
+++ feature.py
@@ -8,10 +8,7 @@ import knn_classifier as knn_classifier
 
 
 def test_image(test_image):
-
-    # load the image
     image = test_image
-
     chans = cv2.split(image)
     colors = ('b', 'g', 'r')
     features = []
@@ -19,13 +16,9 @@ def test_image(test_image):
     counter = 0
     for (chan, color) in zip(chans, colors):
         counter = counter + 1
-
         hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
         features.extend(hist)
-
-        # find the peak pixel values for R, G, and B
         elem = np.argmax(hist)
-
         if counter == 1:
             blue = str(elem)
         elif counter == 2:
@@ -34,22 +27,14 @@ def test_image(test_image):
             red = str(elem)
             feature_data = red + ',' + green + ',' + blue
             print(feature_data)
-
     with open('test.data', 'w') as myfile:
         myfile.write(feature_data)
-
-
 def training_image(img_name):
-
-    # detect image color by using image file name to label training data
     if 'royal_blue' in img_name:
         data_source = 'royal_blue'
     elif 'royal_red' in img_name:
         data_source = 'royal_red'
-
-    # load the image
     image = cv2.imread(img_name)
-
     chans = cv2.split(image)
     colors = ('b', 'g', 'r')
     features = []
@@ -60,8 +45,6 @@ def training_image(img_name):
 
         hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
         features.extend(hist)
-
-        # find the peak pixel values for R, G, and B
         elem = np.argmax(hist)
 
         if counter == 1:
@@ -71,16 +54,10 @@ def training_image(img_name):
         elif counter == 3:
             red = str(elem)
             feature_data = red + ',' + green + ',' + blue
-
     with open('training.data', 'a') as myfile:
         myfile.write(feature_data + ',' + data_source + '\n')
-
-
 def training():
-
-    # red color training images
     for f in os.listdir('./training_dataset/royal_blue'):
         training_image('./training_dataset/royal_blue/' + f)
-
     for f in os.listdir('./training_dataset/royal_red'):
         training_image('./training_dataset/royal_red/' + f)
